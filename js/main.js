bafc54dc25084fc3c9b1d9626c36b0ad86b7cf76
sumiship <osumi.k.osumi.k@gmail.com>
2021-05-16 06:11:50 +0900

    manymany functions

--- js/main.js
+++ js/main.js
@@ -48,9 +48,10 @@
       ],
       attackNum: [, , , ,],
       selectedNum: null,
-      attackAble: false,
+      isFinished: false,
       answer: "",
       count: 0,
+      res: [],
     },
     methods: {
       putNum: function (num) {
@@ -60,13 +61,10 @@
         }
         this.numList[this.selectedNum].able = false;
         this.selectedNum = null;
-        // console.log(this.numList[this.selectedNum].able);
-        // console.log("attackNum:" + this.attackNum);
       },
       selectNum: function (num) {
         if (this.numList[num].able) {
           this.selectedNum = num;
-          console.log("selectedNum:" + this.selectedNum);
         }
       },
       makeArr: function () {
@@ -82,7 +80,6 @@
           )[0];
         }
         this.answer = arr;
-        console.log(arr);
       },
       judge: function (source, arr) {
         let hit = 0;
@@ -103,84 +100,49 @@
         return [hit, blow];
       },
       check: function () {
+        if (
+          this.attackNum[0] == undefined ||
+          this.attackNum[1] == undefined ||
+          this.attackNum[2] == undefined ||
+          this.attackNum[3] == undefined
+        )
+          return;
         this.count += 1;
         const text = this.attackNum;
-        const li = document.createElement("li");
+        // const li = document.createElement("li");
         const ans = this.judge(this.answer, text);
         if (ans[0] === 4) {
-          li.textContent = `${text}   hit:${ans[0]} blow:${ans[1]}  CHECKMATE!!!!`;
-          document.querySelector(
-            "h1"
-          ).textContent = `${this.count}回でクリア！！！！！！！！`;
+          this.isFinished = true;
         } else {
-          li.textContent = `${text}  　hit:${ans[0]} blow:${ans[1]}`;
+          // li.textContent =
+          //   text + "<br>" + `${text}  　hit:${ans[0]} blow:${ans[1]}`;
         }
-        document.querySelector("ul").appendChild(li);
+        text[4] = ans[0];
+        text[5] = ans[1];
+        this.res[this.count - 1] = text;
+        // document.querySelector("ul").appendChild(li);
         this.reset();
+      },
+      viewAns: function () {
+        this.isFinished = true;
+      },
+      restart: function () {
+        this.reset();
+        this.makeArr();
+        this.isFinished = false;
+        this.res = [];
+        this.count = 0;
       },
       reset() {
         for (let i = 0; i < 10; i++) {
           this.numList[i].able = true;
-          this.attackNum = [, , , ,];
-          this.selectedNum = null;
         }
+        this.attackNum = [, , , ,];
+        this.selectedNum = null;
       },
     },
     mounted: function () {
       this.makeArr();
     },
   });
-
-  // function makeArr() {
-  //   const source = [];
-  //   const arr = [];
-  //   for (let i = 0; i < 10; i++) {
-  //     source[i] = i;
-  //   }
-  //   for (let i = 0; i < 4; i++) {
-  //     arr[i] = source.splice(Math.floor(Math.random() * source.length), 1)[0];
-  //   }
-  //   return arr;
-  // }
-
-  // function judge(source, arr) {
-  //   let hit = 0;
-  //   let blow = 0;
-  //   for (let i = 0; i < 4; i++) {
-  //     for (let t = 0; t < 4; t++) {
-  //       if (i === t) {
-  //         if (source[i] === arr[t]) {
-  //           hit += 1;
-  //         }
-  //       } else {
-  //         if (source[i] === arr[t]) {
-  //           blow += 1;
-  //         }
-  //       }
-  //     }
-  //   }
-  //   return [hit, blow];
-  // }
-
-  // function check(arr) {
-  //   let count = 0;
-  //   document.querySelector("button").addEventListener("click", () => {
-  //     count += 1;
-  //     const text = [];
-  //     const li = document.createElement("li");
-  //     for (let i = 0; i < 4; i++) {
-  //       text[i] = document.querySelectorAll("select")[i].value - 0;
-  //     }
-  //     const ans = judge(arr, text);
-  //     if (ans[0] === 4) {
-  //       li.textContent = `${text}   hit:${ans[0]} blow:${ans[1]}  CHECKMATE!!!!`;
-  //       document.querySelector(
-  //         "h1"
-  //       ).textContent = `${count}回でクリア！！！！！！！！`;
-  //     } else {
-  //       li.textContent = `${text}   　　　　hit:${ans[0]} blow:${ans[1]}`;
-  //     }
-  //     document.querySelector("ul").appendChild(li);
-  //   });
-  // }
 }
